55471e5b15d2ac1f747ac247bc20da2c58f9f824
melnicenkovadik <melnicenkovadik@gmail.com>
2023-12-18 19:52:26 +0100

    add visual tool, script, fix screens

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -4,6 +4,7 @@ import cls from './Button.module.scss';
 
 export enum ThemeButton {
     CLEAR = 'clear',
+    CLEAR_INVERTED = 'clear_inverted',
     OUTLINE = 'outline',
 }
 
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,9 +1,11 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback } from 'react';
 import { cn } from 'shared/lib/classNames/classNames';
 import { AppLink } from 'shared/ui/AppLink';
 import { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
 import { routeConfig } from 'shared/config/routeConfig/route.config';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
 import s from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -14,15 +16,50 @@ interface NavbarProps {
 export const Navbar: FC<NavbarProps> = (props) => {
     const { className } = props;
     const { t } = useTranslation();
+    const [isAuthModalOpen, setIsAuthModalOpen] = React.useState(false);
+
+    const onCloseModal = useCallback(() => {
+        setIsAuthModalOpen((prev) => !prev);
+    }, []);
+
     return (
         <div className={cn(s.navbar, [className])}>
+            <Modal
+                isOpen={isAuthModalOpen}
+                onClose={onCloseModal}
+            >
+                <div className={cn(s.modal)}>
+                    <div className={cn(s.modalContent)}>
+                        <div className={cn(s.modalTitle)}>
+                            {t('modalTitle')}
+                        </div>
+                        <div className={cn(s.modalText)}>
+                            {t('modalText')}
+                        </div>
+                        <div className={cn(s.modalBtns)}>
+                            <Button
+                                theme={ThemeButton.CLEAR}
+                                onClick={onCloseModal}
+                            >
+                                {t('modalBtnOk')}
+                            </Button>
+                            <Button
+                                theme={ThemeButton.OUTLINE}
+                                onClick={onCloseModal}
+                            >
+                                {t('modalBtnCancel')}
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </Modal>
             <div className={cn(s.links)}>
-                <AppLink theme={AppLinkTheme.SECONDARY} to={routeConfig.main.path}>
-                    {t('mainPage')}
-                </AppLink>
-                <AppLink theme={AppLinkTheme.SECONDARY} to={routeConfig.about.path}>
-                    {t('aboutPage')}
-                </AppLink>
+                <Button
+                    theme={ThemeButton.CLEAR_INVERTED}
+                    onClick={onCloseModal}
+                >
+                    {t('openModal')}
+                </Button>
             </div>
         </div>
     );
